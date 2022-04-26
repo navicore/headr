109088c3dc7cfcb5a4f39b3da3f7c9440077bdb2
Ed Sweeney <ed@onextent.com>
2022-04-26 07:56:46 -0700

    init

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,45 @@ pub struct Config {
     lines: usize,
     bytes: Option<usize>,
 }
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("headr")
+        .version("0.1.0")
+        .author("Ed Sweeney <ed@onextent.com>")
+        .about("Rust head")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input files")
+                .default_value("-")
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("n")
+                .long("lines")
+                .help("Number lines")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("b")
+                .long("bytes")
+                .help("Number bytes")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        //lines: matches.value_of("lines").unwrap(),
+        lines: 10,
+        //bytes: matches.value_of("bytes").unwrap(),
+        bytes: None,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:#?}", config);
+    Ok(())
+}
